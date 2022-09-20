25ff2a8628d99c70288985d19de92b62a4463ecd
Nartyna <mplutwska@gmail.com>
2022-09-20 22:04:38 +0200

    Add events singletone and camera shake on damage

--- Player/PlayerStats.gd
+++ Player/PlayerStats.gd
@@ -7,6 +7,8 @@ var max_health = 4
 var health = max_health setget set_health
 
 func set_health(new_value):
+	if new_value < health:
+		Events.emit_signal("add_screenshake", 0.4, 0.5)
 	health = clamp(new_value, 0, max_health)
 	if health == 0:
 		emit_signal("player_death")
