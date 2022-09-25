4e8807c170a46517eb471ba82008afb0b1338d02
Nartyna <mplutwska@gmail.com>
2022-09-25 22:11:25 +0200

    Add controler support and jumping with spacebar

--- Player/PlayerGun.gd
+++ Player/PlayerGun.gd
@@ -1,5 +1,16 @@
 extends Node2D
 
-func _process(_delta):
-	var player = get_parent()
-	rotation = player.get_local_mouse_position().angle()
+func _input(event):
+	var rs_look = Vector2(0,0)
+	var deadzone = 0.3
+	var ignore_mouse = false
+	if event is InputEventJoypadMotion:
+		ignore_mouse = true
+		rs_look.y = Input.get_joy_axis(0, JOY_AXIS_3)
+		rs_look.x = Input.get_joy_axis(0, JOY_AXIS_2)
+		if rs_look.length() >= deadzone:
+			rotation_degrees = rad2deg(rs_look.angle())
+	elif event is InputEventMouseMotion and !ignore_mouse:
+		ignore_mouse = false
+		var player = get_parent()
+		rotation = player.get_local_mouse_position().angle()
