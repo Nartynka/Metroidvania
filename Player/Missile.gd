4c170c637e27c16277914fa1f7ef5ab1871386ba
Nartyna <mplutwska@gmail.com>
2022-10-01 10:04:58 +0200

    add bircks that player can destroy using missiles

---
+++ Player/Missile.gd
@@ -0,0 +1,10 @@
+extends "res://Player/Projectile.gd"
+
+var EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
+const BRICK_LAYER_BIT = 4
+
+func _on_Hitbox_body_entered(body):
+	if body.get_collision_layer_bit(BRICK_LAYER_BIT):
+		Utils.instance_on_main(EnemyDeathEffect, body.global_position + Vector2(8,8))
+		body.queue_free()
+	._on_Hitbox_body_entered(body)
