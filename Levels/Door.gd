fd3a0e4ca141b548f1d4db5bddf623d8045c9302
Nartyna <mplutwska@gmail.com>
2022-10-06 19:42:49 +0200

    Create connection between level 01 and main world (level_free)

--- Levels/Door.gd
+++ Levels/Door.gd
@@ -6,7 +6,4 @@ export(String, FILE, "*.tscn") var new_level_path = ""
 var active = true
 
 func _on_Door_body_entered(player):
-	if active:
-		print("aaaa")
-		player.emit_signal("door_entered", self)
-		active = false
+	player.emit_signal("door_entered", self)
