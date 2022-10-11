97a4873065fed187e4748d917bfd1e01bb0033a4
Nartyna <mplutwska@gmail.com>
2022-10-11 22:16:46 +0200

    add boss movemnt and boss bullets

---
+++ Enemies/BossEnemy.gd
@@ -0,0 +1,37 @@
+extends "res://Enemies/Enemy.gd"
+
+onready var leftWallCheck = $LeftWallCheck
+onready var rightWallCheck = $RightWallCheck
+
+const Bullet = preload("res://Enemies/EnemyBullet.tscn")
+export(int) var ACCELERATION = 70
+
+signal boss_died
+
+func _ready():
+	pass
+	
+func _process(delta):
+	chase_player(delta)
+
+func chase_player(delta):
+	var player = Utils.get_player()
+	if player != null:
+		var direction = sign(player.global_position.x - global_position.x)
+		motion.x += ACCELERATION * delta * direction
+		motion.x = clamp(motion.x, -MAX_SPEED, MAX_SPEED)
+		global_position += motion * delta
+		rotation_degrees = lerp(rotation_degrees, (motion.x / MAX_SPEED), 0.3)
+		
+		if (rightWallCheck.is_colliding() or leftWallCheck.is_colliding()) and motion.x != 0:
+			motion.x *= -0.5
+
+func fire_bullet():
+	var bullet = Utils.instance_on_main(Bullet, global_position)
+	var velocity = Vector2.DOWN * 50
+	velocity = velocity.rotated(deg2rad(rand_range(-30, 30)))
+	bullet.velocity = velocity
+	
+
+func _on_FireTimer_timeout():
+	fire_bullet()
