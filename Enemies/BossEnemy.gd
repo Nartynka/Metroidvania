aee58c2db3024317ef593e523b2b7d6ed475d5d5
Nartyna <mplutwska@gmail.com>
2022-10-12 21:19:14 +0200

    fix boss animation and movement

--- Enemies/BossEnemy.gd
+++ Enemies/BossEnemy.gd
@@ -20,7 +20,7 @@ func chase_player(delta):
 		var direction = sign(player.global_position.x - global_position.x)
 		motion.x += ACCELERATION * delta * direction
 		motion.x = clamp(motion.x, -MAX_SPEED, MAX_SPEED)
-		global_position += motion * delta
+		global_position.x += motion.x * delta
 		rotation_degrees = lerp(rotation_degrees, (motion.x / MAX_SPEED), 0.3)
 		
 		if (rightWallCheck.is_colliding() or leftWallCheck.is_colliding()) and motion.x != 0:
@@ -29,7 +29,7 @@ func chase_player(delta):
 func fire_bullet():
 	var bullet = Utils.instance_on_main(Bullet, global_position)
 	var velocity = Vector2.DOWN * 50
-	velocity = velocity.rotated(deg2rad(rand_range(-30, 30)))
+	velocity = velocity.rotated(deg2rad(rand_range(-40, 40)))
 	bullet.velocity = velocity
 	
 
